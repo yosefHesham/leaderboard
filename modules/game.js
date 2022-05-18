8413020206fa7665af8b442395e9913f18f0805a
yosefHesham <yousefmobdev@gmail.com>
2022-05-18 10:59:15 +0200

    Create api service

---
+++ modules/game.js
@@ -0,0 +1,8 @@
+class User {
+  constructor(name,score,id) {
+      this.name = name;
+      this.score = score;
+  }
+}
+
+export default User
---
+++ modules/leaderboard_service.js
@@ -0,0 +1,25 @@
+import User from "./game";
+import { toJson } from "./json";
+
+class LeaderBoardService {
+  static basUrl = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/";
+  static gameId = ""
+  static getData = async () => {
+    try {
+    const resp = await fetch(`${this.basUrl}/games/${this.gameId}/scores`);
+    return resp
+    }catch(e) {
+      throw e;
+    }
+  };
+
+
+  /** @param {User} user */
+  static sendData = async (user) => {
+    const req = new XMLHttpRequest();
+    req.open("POST",`${this.basUrl}/games/${this.gameId}/score`);
+    req.send(toJson(user));
+  }
+}
+
+export default LeaderBoardService;
