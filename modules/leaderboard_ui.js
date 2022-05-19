7ce2ffec4e98fb538f7b6f2f9b0ef04692d65d95
yosefHesham <yousefmobdev@gmail.com>
2022-05-19 11:14:38 +0200

    Remove console.log

--- modules/leaderboard_ui.js
+++ modules/leaderboard_ui.js
@@ -7,17 +7,16 @@ const refreshButton = document.querySelector('.refresh-button');
 
 const submitButton = document.querySelector('.submit');
 
- /** @param {Array} data */
+/** @param {Array} data */
 const sortScores = (data) => {
-   const sortedData=  data.sort((a,b) => Number(b.score)- Number(a.score));
-   console.log(sortedData);
-   return sortedData;
-}
+  const sortedData = data.sort((a, b) => Number(b.score) - Number(a.score));
+  return sortedData;
+};
 
 const renderItems = async () => {
   const result = await LeaderBoardService.getData();
   /** @type {Array} */
-  let basic = result.result;
+  const basic = result.result;
   const data = sortScores(basic);
   for (let i = 0; i < data.length; i += 1) {
     const element = createElement(data[i], i % 2 === 0);
